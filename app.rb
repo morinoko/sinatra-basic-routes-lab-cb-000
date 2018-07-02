d64f7b6e49eb850dfab04ccde4fd52f1cd3c2296
Felice Forby <feliceforby@gmail.com>
2018-07-02 23:11:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Felice"
+  end
+
+  get '/hometown' do
+    "My hometown is Chillicothe, OH (and Stuttgart!)"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 1950 by King Princess"
+  end
 end
